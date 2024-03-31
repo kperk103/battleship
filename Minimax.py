521e3f39704144431c40326cf7476ff830202795
TrevorM00re <133248538+TrevorM00re@users.noreply.github.com>
2024-03-31 13:52:51 -0400

    minimax/player updates

---
+++ Minimax.py
@@ -0,0 +1,240 @@
+import Gameboard
+import copy
+
+#connect 4 minimax
+
+#from gameboard.py
+COLUMNS = 7
+ROWS = 6
+EMPTY = '-'  # 0
+PLAYER1 = 'X'  # 1
+PLAYER2 = '0'  # 2
+
+
+
+class Minimax:
+
+    def __init__(self, depth=float('inf')):
+        self.depth = depth
+
+
+
+    #integrate getCols() function, takes a boardstate, and returns a list of the
+    #columns in the board
+    def getCols(self, currState):
+        numColumns = len(currState[0])
+        columns = [[row[col] for row in currState] for col in range(numColumns)]
+        return columns
+
+    #integrate getRows() function, takes a boardstate, and returns a list of the
+    #columns in the board
+    def getRows(self, currState):
+        rows = [row for row in currState]
+        return rows
+
+
+    #integrate checkWin() function, takes a boardstate, and returns the mark 
+    #of whoever won if there is a winner, 0 if the game is still ongoing, 
+    #or -1 if the game ended with no winner
+    def checkWin(self, currState): 
+        winner = 0
+        # check if win in column
+        for col in range(COLUMNS):
+            for row in range(ROWS-3):
+                if currState[row][col] != 0 and currState[row][col] == currState[row+1][col] == currState[row+2][col] == currState[row+3][col]:
+                    winner = currState[row][col]
+                    break
+        # check if win in row
+        for col in range(COLUMNS - 3):
+            for row in range(ROWS):
+                if currState[row][col] != 0 and currState[row][col] == currState[row][col+1] == currState[row][col+2] == currState[row][col+3]:
+                    winner = currState[row][col]
+                    break
+        # check if win in positive diagonal
+        for col in range(COLUMNS - 3):
+            for row in range(ROWS - 3):
+                if currState[row][col] != 0 and currState[row][col] == currState[row+1][col+1] == currState[row+2][col+2] == currState[row+3][col+3]:
+                    winner = currState[row][col]
+                    break
+        # check if win in negative diagonal
+        for col in range(COLUMNS - 3):
+            for row in range(3, ROWS):
+                if currState[row][col] != 0 and currState[row][col] == currState[row-1][col+1] == currState[row-2][col+2] == currState[row-3][col+3]:
+                    winner = currState[row][col]
+                    break
+        if winner != 0:
+            return winner
+        else: 
+            chips_left = 0
+            for row in currState:
+                for element in row:
+                    if element != PLAYER1 and element != PLAYER2:
+                        chips_left += 1
+    
+            return 0 if chips_left > 0 else -1
+
+    
+    #integrate getValid() function, takes a boardstate, and returns a 1d array 
+    #of size 7 of all the valid columns a piece can be dropped on. 
+    #1 at the column index if a piece can be dropped, 0 if it cannot.
+    def getValid(self, currState):
+        transposedArray = [[row[col] for row in currState] for col in range(len(currState[0]))]
+
+        validArray = []
+
+        # Iterate through columns
+        for column in transposedArray:
+            row = 5
+            while (column[row] != 0 or column[row] != 0) and row >= 0:
+                row -= 1
+
+            if row < 0:
+                validArray.append(0)
+            else:
+                validArray.append(1)
+
+        return validArray
+
+        
+
+
+    #takes a boardstate array, and returns a new boardstate array with the specified 
+    #piece dropped into the specified column
+    def dropPiece(self, currState, piece, column):
+        row = 5
+
+        newState = copy.deepcopy(currState)
+
+        while currState[row][column] != 0 or currState[row][column] != 0:
+            row -= 1
+
+        newState[row][column] = piece
+
+        return newState
+
+
+
+
+    #evaluates a score for a window array of 4 pieces. 
+    def evaluateWindow(self, window, piece):
+        score = 0
+
+        #its better to just win than to block
+        if window.count(piece) == 4:
+            score += 100
+        elif window.count(piece) == 3 and window.count(EMPTY) == 1:
+            score += 10
+        elif window.count(piece) == 2 and window.count(EMPTY) == 2:
+            score += 5
+        elif window.count(piece) == 1 and window.count(EMPTY) == 3:
+            score += 2
+        elif window.count(piece) == 0 and window.count(EMPTY) == 3:
+            score -= 1
+        elif window.count(piece) == 0 and window.count(EMPTY) == 2:
+            score -= 4
+        elif window.count(piece) == 0 and window.count(EMPTY) == 1:
+            score -= 9
+        elif window.count(piece) == 0 and window.count(EMPTY) == 0: 
+            score -= 99
+        
+        return score
+
+
+
+    #returns an integer score based on the currState of the game, 
+    #and a given piece. (the piece's score will contribute to positive score
+    #opponents piece contributing to negative score)
+    def scorePos(self, currState, piece):
+        score = 0
+
+        #score columns
+        for column in self.getCols(currState):
+            for i in range(ROWS - 3):
+                window = column[i:i+4]
+                score += self.evaluateWindow(window, piece)
+
+        #score rows
+        for row in self.getRows(currState):
+            for i in range(COLUMNS - 3):
+                window = row[i:i+4]
+                score += self.evaluateWindow(window, piece)
+
+        #score positive sloped diagonal
+        for r in range(ROWS - 3):
+            for c in range(COLUMNS - 3):
+                window = [currState[r+i][c+i] for i in range(4)]
+                score += self.evaluateWindow(window, piece)
+
+        #score negative sloped diagonal
+        for r in range(ROWS - 3):
+            for c in range(COLUMNS - 3):
+                window = [currState[r+3-i][c+i] for i in range(4)]
+                score += self.evaluateWindow(window, piece)
+
+        return score
+
+
+
+    #parameters: currState, a two dimensional array with 6 rows, 7 cols,  
+    # containing the current state of the game
+    #currPlayer, the player who's turn it is currently.
+    #returns a tuple with the column to drop the piece, and the heuristic value
+    def minimax(self, currState, currPlayer):
+        return self.miniHelper(currState, currPlayer, currPlayer, 0, float('-inf'), float('inf'))[0]
+    
+    def miniHelper(self, currState, currPlayer, maximizer, depth, alpha, beta):
+        #check for game over or tie, assign points accordingly
+        didWin = self.checkWin(currState)
+        if didWin != 0: 
+            if didWin == PLAYER1: 
+                return (None, float('inf')) if maximizer == PLAYER1 else (None, float('-inf'))
+            elif didWin == PLAYER2: 
+                return (None, float('inf')) if maximizer == PLAYER2 else (None, float('-inf'))
+            else: 
+                return (None, 0) #game ended in a tie
+            
+        #check if we have reached past the max depth
+        if depth > self.depth:
+            return (None, self.scorePos(currState, maximizer))
+
+        #one dimensional array of length 7 filled with 1 if a disc can be placed 
+        #on that column, 0 if not integrate getValid() method
+        validDrop = self.getValid(currState)
+        nextPlayer = PLAYER1 if currPlayer == PLAYER2 else PLAYER2
+
+        #maximize or minimize
+        val = float('-inf') if currPlayer == maximizer else float('inf')
+        col = -1
+        for i in range(len(validDrop)):
+            if validDrop[i] == 1:
+                updated = self.dropPiece(currState, currPlayer, i)
+                score = self.miniHelper(updated, nextPlayer, maximizer, depth + 1, alpha, beta)[1]
+
+                if currPlayer == maximizer: 
+                    if score > val: 
+                        val = score
+                        col = i
+                    alpha = max(alpha, val)
+                else: 
+                    if score < val: 
+                        val = score
+                        col = i
+                    beta = min(beta, val)
+
+                if alpha >= beta: 
+                        break
+        
+        return col, val
+    
+
+
+
+'''
+example usage: 
+
+ai = Minimax()
+mini1 = Minimax(3)  #<--- creates a minimax class object, with maximum search depth of length 3
+mini2 = Minimax() #<--- creates a minimax class object with no maximhm search depth
+
+mini1.minimax(currState, currPlayer) #<-- returns an optimal column to drop, as well as the heuristic val
+'''
--- Player.py
+++ Player.py
@@ -1,7 +1,9 @@
 from Gameboard import Gameboard
+from Minimax import Minimax
 
 if __name__ == "__main__":
     game = Gameboard()
+    mini = Minimax(1)
     turn_number = -1 # Turns start on turn # 0.
 
     print("Welcome to Connect 4!")
@@ -14,8 +16,13 @@ if __name__ == "__main__":
 
         while invalid_input:
             game.toString()
-            player = (turn_number % 2) + 1
-            col = input(f"Player {player} ({'X' if player == 1 else '0'}), Select a column (1-7): ")
+            player = (turn_number % 2) + 1 
+
+            col = 0
+            if player == 1:
+                col = input(f"Player {player} ({'X' if player == 1 else '0'}), Select a column (1-7): ")
+            else:
+                col = mini.minimax(game.board, 2) + 1
             print("\n\n\n---------------------------------------------------")
             
             try:
